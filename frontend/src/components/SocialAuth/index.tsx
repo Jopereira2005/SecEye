ff4bf8266bf01d95e5e8d7b0b5517002034a4b21
João Augusto Pereira Vieira <jopereiravieira98@gmail.com>
2026-05-20 16:51:39 -0300

    ✨feat(home): roteamento teste

--- frontend/src/components/SocialAuth/index.tsx
+++ frontend/src/components/SocialAuth/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { use } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { Image } from 'expo-image';
 import { styles } from './_social-auth.styles';
+import { useRouter } from 'expo-router';
 
 export function SocialAuth() {
+  const router = useRouter();
+
   return (
     <View>
       <View style={styles.divider}>
@@ -13,12 +16,12 @@ export function SocialAuth() {
       </View>
 
       <View style={styles.socialContainer}>
-        <TouchableOpacity style={styles.socialButton}>
+        <TouchableOpacity style={styles.socialButton} onPress={() => router.push('/home' as any)}>
           <Image source={require('@/assets/svgs/google-icon.svg')} style={styles.socialIcon} />
           <Text style={styles.socialText}>Google</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.socialButton}>
+        <TouchableOpacity style={styles.socialButton} onPress={() => router.push('/home' as any)}>
           <Image source={require('@/assets/svgs/biometrics-icon.svg')} style={styles.socialIcon} />
           <Text style={styles.socialText}>Biometria</Text>
         </TouchableOpacity>
